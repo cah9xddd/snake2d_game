f7349255a872a5481c9d44c72e738e54816b54ef
Oleksandr Horetskyi <81568005+cah9xddd@users.noreply.github.com>
2023-02-18 04:39:59 +0100

    everything is working

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
-#include <SDL.h>
 #include "Engine/Engine.h"
-int main(int argc, char *argv[])
-{  
-    Engine E1;
-    E1.init();
-    SDL_Quit();
+#include <SDL.h>
+Engine* engine = nullptr;
+int main(int argc, char* argv[])
+{
+    engine = new Engine();
+    if (!engine->Init("GAME", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 450, SDL_WINDOW_SHOWN ))
+    {
+        SDL_LogCritical(1, "Failed initialize\n");
+    }
+    else
+    {
+        if (!engine->LoadMedia())
+        {
+            SDL_LogCritical(1, "Failed to load media!\n");
+        }
+    }
+
+    while (engine->Running())
+    {
+        engine->HandleEvents();
+
+        engine->Update();
+
+        engine->Render();
+    }
+
+    engine->Clean();
+    engine->Close();
     return 0;
 }
